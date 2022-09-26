62a8b58fb16a50cab2370e2ee4e457f587978593
EditaPyrkh <113599547+EditaPyrkh@users.noreply.github.com>
2022-09-26 13:14:44 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,12 @@
+var p = document.querySelector('p')
+    let input = document.querySelector('input');
+    paragraph.textContent = input.value;
+
+   
+    function alerted(){
+      
+      alert(`Confirmed! Thank you, ${input.value}.`);
+    } 
+    
+    
+    
